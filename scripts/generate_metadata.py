2896182fb3954101141fb69dff07c9ef653b549d
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-08-07 15:25:16 +0800

    Remove copyright comments; Change cmd description.

--- scripts/generate_metadata.py
+++ scripts/generate_metadata.py
@@ -1,17 +1,3 @@
-# Copyright 2020 Huy Le Nguyen (@usimarit)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 import argparse
 from tensorflow_asr.configs.config import Config
@@ -20,7 +6,7 @@ from tensorflow_asr.datasets.asr_dataset import ASRDataset
 from tensorflow_asr.featurizers.speech_featurizers import TFSpeechFeaturizer
 from tensorflow_asr.featurizers.text_featurizers import SubwordFeaturizer, SentencePieceFeaturizer
 
-parser = argparse.ArgumentParser(prog="TFRecords Creation")
+parser = argparse.ArgumentParser(prog="Dataset Metadata Creation")
 
 parser.add_argument("--stage", type=str, default="train", help="The stage of dataset")
 
