1c10d5c2c1438af7a39b97727d1b5561b6b1df82
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-09-02 14:24:35 +0800

    Remove copyright comments. Add change comment.

--- examples/demonstration/tflite_conformer.py
+++ examples/demonstration/tflite_conformer.py
@@ -1,21 +1,9 @@
-# Copyright 2020 Huy Le Nguyen (@usimarit)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import argparse
 import tensorflow as tf
 
 from tensorflow_asr.featurizers.speech_featurizers import read_raw_audio
+
+# XXX: the function of this module is same as that of run_tflite_model.py
 
 parser = argparse.ArgumentParser(prog="Conformer non streaming")
 
